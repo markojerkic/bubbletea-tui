e7786b3389a21ed043e4c4a7c565156398652a2b
Marko Jekrić <markojerkic266@gmail.com>
2024-02-13 19:22:42 +0100

    progress

--- register/register.go
+++ register/register.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/charmbracelet/bubbles/textinput"
+	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
@@ -12,20 +13,27 @@ type (
 )
 
 type model struct {
-	textInput textinput.Model
-	err       error
+	viewPort         viewport.Model
+	textInput        textinput.Model
+	selectedUserName chan string
+	err              error
 }
 
 func InitRegister() model {
 	ti := textinput.New()
-	ti.Placeholder = "Pikachu"
+	ti.Placeholder = "Username..."
 	ti.Focus()
 	ti.CharLimit = 156
 	ti.Width = 20
 
+	vp := viewport.New(50, 5)
+	vp.SetContent("Welcome to my chat app! Please enter your username and press enter to continue.")
+
 	return model{
-		textInput: ti,
-		err:       nil,
+		selectedUserName: make(chan string),
+		viewPort:         vp,
+		textInput:        ti,
+		err:              nil,
 	}
 }
 
@@ -34,13 +42,24 @@ func (m model) Init() tea.Cmd {
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var cmd tea.Cmd
+	var (
+		tCmd tea.Cmd
+		vCmd tea.Cmd
+	)
+
+	m.textInput, tCmd = m.textInput.Update(msg)
+	m.viewPort, vCmd = m.viewPort.Update(msg)
 
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.Type {
-		case tea.KeyEnter, tea.KeyCtrlC, tea.KeyEsc:
+		case tea.KeyCtrlC, tea.KeyEsc:
 			return m, tea.Quit
+		case tea.KeyEnter:
+			// m.selectedUserName <- m.textInput.Value()
+			m.viewPort.SetContent(fmt.Sprintf("Welcome, %s!", m.textInput.Value()))
+			m.textInput.Reset()
+			m.viewPort.GotoBottom()
 		}
 
 	// We handle errors just like any other message
@@ -49,14 +68,13 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		return m, nil
 	}
 
-	m.textInput, cmd = m.textInput.Update(msg)
-	return m, cmd
+	return m, tea.Batch(tCmd, vCmd)
 }
 
 func (m model) View() string {
 	return fmt.Sprintf(
-		"What’s your favorite Pokémon?\n\n%s\n\n%s",
+		"%s\n%s",
+		m.viewPort.View(),
 		m.textInput.View(),
-		"(esc to quit)",
-	) + "\n"
+	)
 }
